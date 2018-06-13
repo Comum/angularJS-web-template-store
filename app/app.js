c6b35f00e8044159d5e021faf321b9f2a7c1f6ad
Comum <miguel.rib.20@gmail.com>
2018-06-13 23:32:25 +0100

    Adding routes

--- app/app.js
+++ app/app.js
@@ -10,5 +10,5 @@ angular.module('templateStore', [
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/templates'});
 }]);
