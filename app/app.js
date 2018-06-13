d287102779ada9999904977778a10eb749038080
Comum <miguel.rib.20@gmail.com>
2018-06-13 22:57:02 +0100

    Set up project

---
+++ app/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('templateStore', [
+  'ngRoute',
+  'templateStore.templates',
+  'templateStore.view1',
+  'templateStore.view2'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
